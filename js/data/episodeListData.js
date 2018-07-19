e648977b92fd68c436c6fe2a325f73b6c274f3f4
Max Beyer <inflatableanchor@gmail.com>
2018-07-19 13:49:42 -0500

    credit change

--- js/data/episodeListData.js
+++ js/data/episodeListData.js
@@ -2,7 +2,7 @@ const episodeData = [
   {
     title: "Episode 2",
     date: "July 12, 2018",
-    description: "Lefty wonders about the possibilities of an artifact he found, but he needs his friends Corny Falco and Pavlovena to help identify what it is and what it does. Meanwhile the people of the Ark decide to build defenses to protect against the dangers of The Zone, when trouble strikes!<br><br>Original music composed by Max Beyer, and a big thanks to these other artists for their music found at freesound.org:<br><br>Sirkoto51 - Anime Fight Music Loop <br><br> #1 Neehnahw - Konflict",
+    description: "Lefty wonders about the possibilities of an artifact he found, but he needs his friends Corny Falco and Pavlovena to help identify what it is and what it does. Meanwhile the people of the Ark decide to build defenses to protect against the dangers of The Zone, when trouble strikes!<br><br>Original music composed by Max Beyer, and a big thanks to these other artists for their music found at freesound.org:<br><br>Sirkoto51 - Anime Fight Music Loop <br><br> neehnahw - Konflict",
     audio: "http://static1.squarespace.com/static/51f14daae4b0e3adff18a86a/t/5b46106b6d2a73ffbacd9539/1531318470818/Episode+2.mp3/original/Episode+2.mp3"
   },
   {
