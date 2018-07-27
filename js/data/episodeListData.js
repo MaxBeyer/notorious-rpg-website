09b5407954ca2c88471893c461093a01b287c563
Max Beyer <inflatableanchor@gmail.com>
2018-07-27 17:34:16 -0500

    added analytics to episode 2 & 1

--- js/data/episodeListData.js
+++ js/data/episodeListData.js
@@ -9,13 +9,13 @@ const episodeData = [
     title: "Episode 2",
     date: "July 12, 2018",
     description: "Lefty wonders about the possibilities of an artifact he found, but he needs his friends Corny Falco and Pavlovena to help identify what it is and what it does. Meanwhile the people of the Ark decide to build defenses to protect against the dangers of The Zone, when trouble strikes!<br><br>Original music composed by Max Beyer, and a big thanks to these other artists for their music found at freesound.org:<br><br>Sirkoto51 - Anime Fight Music Loop <br><br> neehnahw - Konflict",
-    audio: "http://static1.squarespace.com/static/51f14daae4b0e3adff18a86a/t/5b46106b6d2a73ffbacd9539/1531318470818/Episode+2.mp3/original/Episode+2.mp3"
+    audio: "http://media.blubrry.com/thenotoriousrpg/static1.squarespace.com/static/51f14daae4b0e3adff18a86a/t/5b46106b6d2a73ffbacd9539/1531318470818/Episode+2.mp3/original/Episode+2.mp3"
   },
   {
     title: "Episode 1",
     date: "July 12, 2018",
     description: "In this setup episode of The Notorious RPG, 4 Minneapolis friends and improvisers create their mutant characters in a dystopian post-apocalyptic future.  Join in the campaign as Patrick (Lefty), Maggie (Pavlovena) and Brandon (Corny Falco), led by Game Master Max, venture into the unknown.<br><br>Original music composed by Max Beyer.",
-    audio: "http://static1.squarespace.com/static/51f14daae4b0e3adff18a86a/t/5b460f280e2e72b22160548c/1531318139855/Episode+1.mp3/original/Episode+1.mp3"
+    audio: "http://media.blubrry.com/thenotoriousrpg/static1.squarespace.com/static/51f14daae4b0e3adff18a86a/t/5b460f280e2e72b22160548c/1531318139855/Episode+1.mp3/original/Episode+1.mp3"
   }
 ];
 
