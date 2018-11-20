0a29d97767811830f40c09db5175ec90f3afcf60
Max Beyer <inflatableanchor@gmail.com>
2018-11-19 19:22:38 -0600

    updated player to libsyn iframe. Added episode 10. Changed background image to color

--- js/data/episodeListData.js
+++ js/data/episodeListData.js
@@ -1,63 +1,69 @@
 const episodeData = [
   {
+    title: "Episode 10",
+    date: "November 15, 2018",
+    description: "<p>With the threat of Reptilia now neutralized, the gang faces a new obstacle: a group of 8 zone ghouls. Things may not be what they seem though as they continue to learn more about the wasteland beyond the walls of the Gut.</p> <p>Music composed by Max Beyer</p>",
+    audio: `<iframe style="border: none" src="//html5-player.libsyn.com/embed/episode/id/7561586/height/90/theme/custom/autoplay/no/autonext/no/thumbnail/no/preload/no/no_addthis/no/direction/forward/render-playlist/no/custom-color/000000/" height="90" width="100%" scrolling="no"  allowfullscreen webkitallowfullscreen mozallowfullscreen oallowfullscreen msallowfullscreen></iframe>`
+  },
+  {
     title: "Spooky Halloween Episode!",
     date: "October 31, 2018",
     description: "<p>Meghan Wolff (Co-Host of Good Luck High Five) joins our characters as we play this one shot using Dread RPG System!</p> <p>Lefty learned of powerful artifacts being kept in a far away sector on the zone map.  He enlisted the help of Pavlovena and Chip Carmichael to help retrieve them. </p> <p>Music composed by Max Beyer</p>",
-    audio: "http://media.blubrry.com/thenotoriousrpg/static1.squarespace.com/static/51f14daae4b0e3adff18a86a/t/5bda3446562fa73fc150d85f/1541026979643/Episode+10+Final-+louder+music.mp3/original/Episode+10+Final-+louder+music.mp3"
+    audio: `<iframe style="border: none" src="//html5-player.libsyn.com/embed/episode/id/7573226/height/90/theme/custom/autoplay/no/autonext/no/thumbnail/yes/preload/no/no_addthis/no/direction/forward/render-playlist/no/custom-color/000000/" height="90" width="100%" scrolling="no"  allowfullscreen webkitallowfullscreen mozallowfullscreen oallowfullscreen msallowfullscreen></iframe>`
   },
   {
     title: "Episode 9",
     date: "October 18, 2018",
     description: "<p>  The gang learns about their new gearhead allies, as well as newer more fearsome threats in the zone to look out for.  Meanwhile, Reptilia, and for some reason Corny, continue to try to kill Weezle.  What will it be like to bring gearheads back to a community that's in the middle of a purge?</p><p>Music composed by Max Beyer</p>",
-    audio: "http://media.blubrry.com/thenotoriousrpg/static1.squarespace.com/static/51f14daae4b0e3adff18a86a/t/5bc8851a24a694f7707eaa19/1539868021071/Episode+9+-+Final.mp3/original/Episode+9+-+Final.mp3"
+    audio: `<iframe style="border: none" src="//html5-player.libsyn.com/embed/episode/id/7573229/height/90/theme/custom/autoplay/no/autonext/no/thumbnail/no/preload/no/no_addthis/no/direction/forward/render-playlist/no/custom-color/000000/" height="90" width="100%" scrolling="no"  allowfullscreen webkitallowfullscreen mozallowfullscreen oallowfullscreen msallowfullscreen></iframe>`
   },
   {
     title: "Episode 8",
     date: "October 4, 2018",
     description: "<p>An assembly has called for the retrieval of the generator from the theater glowing with devil lights.  However, being forced to team with murderous Reptilia could prove to be difficult.</p> <p> Music composed by Max Beyer</p>",
-    audio: "http://media.blubrry.com/thenotoriousrpg/static1.squarespace.com/static/51f14daae4b0e3adff18a86a/t/5bb66a1cec212d102d42c889/1538681438099/Episode+8.mp3/original/Episode+8.mp3"
+    audio: `<iframe style="border: none" src="//html5-player.libsyn.com/embed/episode/id/7573232/height/90/theme/custom/autoplay/no/autonext/no/thumbnail/no/preload/no/no_addthis/no/direction/forward/render-playlist/no/custom-color/000000/" height="90" width="100%" scrolling="no"  allowfullscreen webkitallowfullscreen mozallowfullscreen oallowfullscreen msallowfullscreen></iframe>`
   },
   {
     title: "Episode 7",
     date: "September 20, 2018",
     description: "<p>The gang deposits their artifacts to the Dawn Vault, and calls an assembly for a special Zone expedition.  Meanwhile, tensions are on the rise between Boss Aquifi and the gearheads of the Ark. </p><p>Music composed by Max Beyer</p>",
-    audio: "http://media.blubrry.com/thenotoriousrpg/static1.squarespace.com/static/51f14daae4b0e3adff18a86a/t/5ba3af7940ec9aa754559ec1/1537454018551/Episode+7+Final.mp3/original/Episode+7+Final.mp3"
+    audio: `<iframe style="border: none" src="//html5-player.libsyn.com/embed/episode/id/7573235/height/90/theme/custom/autoplay/no/autonext/no/thumbnail/no/preload/no/no_addthis/no/direction/forward/render-playlist/no/custom-color/000000/" height="90" width="100%" scrolling="no"  allowfullscreen webkitallowfullscreen mozallowfullscreen oallowfullscreen msallowfullscreen></iframe>`
   },
   {
     title: "Episode 6",
     date: "September 6, 2018",
     description: "<p>Torn between whether to return to the Ark or keep exploring the Zone for more artifacts, the gang opts for more sweet loot.  They also learn that there’s more threats in the Zone than just Zone Ghouls when nature tests them.</p><p>All music composed by Max Beyer</p>",
-    audio: "http://media.blubrry.com/thenotoriousrpg/static1.squarespace.com/static/51f14daae4b0e3adff18a86a/t/5b9131521ae6cf6b8265ed56/1536242071380/Episode+6+Final.mp3/original/Episode+6+Final.mp3"
+    audio: `<iframe style="border: none" src="//html5-player.libsyn.com/embed/episode/id/7573238/height/90/theme/custom/autoplay/no/autonext/no/thumbnail/no/preload/no/no_addthis/no/direction/forward/render-playlist/no/custom-color/000000/" height="90" width="100%" scrolling="no"  allowfullscreen webkitallowfullscreen mozallowfullscreen oallowfullscreen msallowfullscreen></iframe>`
   },
   {
     title: "Episode 5",
     date: "August 24, 2018",
     description: "<p>A mysterious building that glows with devil lights is found in the zone and explored.  Pavlovena gives Crisp special orders, Corny identifies electronic wonders, and Lefty gets distracted hunting a beast through dark hallways.</p><p>Original music composed by Max Beyer.</p>",
-    audio: "http://media.blubrry.com/thenotoriousrpg/static1.squarespace.com/static/51f14daae4b0e3adff18a86a/t/5b800008c2241bb63030f648/1535115349624/Episode+5+Final.mp3/original/Episode+5+Final.mp3"
+    audio: `<iframe style="border: none" src="//html5-player.libsyn.com/embed/episode/id/7573241/height/90/theme/custom/autoplay/no/autonext/no/thumbnail/no/preload/no/no_addthis/no/direction/forward/render-playlist/no/custom-color/000000/" height="90" width="100%" scrolling="no"  allowfullscreen webkitallowfullscreen mozallowfullscreen oallowfullscreen msallowfullscreen></iframe>`
   },
   {
     title: "Episode 4",
     date: "August 9, 2018",
     description: "<p>Lefty, Corny, and Pavlovena venture into the Zone.  They cross a bridge and find their friend Weezle is in trouble.  Meanwhile, a building nearby somehow has electricity.</p><p>Original music composed by Max Beyer, and a big thanks to Neehnahw from freesound.org for the use of their work Konflict.</p>",
-    audio: "http://media.blubrry.com/thenotoriousrpg/static1.squarespace.com/static/51f14daae4b0e3adff18a86a/t/5b6c4f460ebbe85efff45ca4/1533824914396/Episode+4+Final.mp3/original/Episode+4+Final.mp3"
+    audio: `<iframe style="border: none" src="//html5-player.libsyn.com/embed/episode/id/7573244/height/90/theme/custom/autoplay/no/autonext/no/thumbnail/no/preload/no/no_addthis/no/direction/forward/render-playlist/no/custom-color/000000/" height="90" width="100%" scrolling="no"  allowfullscreen webkitallowfullscreen mozallowfullscreen oallowfullscreen msallowfullscreen></iframe>`
   },
   {
     title: "Episode 3",
     date: "July 27, 2018",
     description: "Corny Falco gets stuck in his suit, Pavlovena has a run in with her favorite enforcer, and Lefty feels guilty after pocketing the direction finder instead of securing it in the Dawn Vault. He attempts to recruit his friends to venture out into the Zone to find more items of value to make up for his perceived selfishness.",
-    audio: "http://media.blubrry.com/thenotoriousrpg/static1.squarespace.com/static/51f14daae4b0e3adff18a86a/t/5b5c7c081ae6cf84851a7164/1532787797843/Episode+3+%281%29.mp3/original/Episode+3+%281%29.mp3"
+    audio: `<iframe style="border: none" src="//html5-player.libsyn.com/embed/episode/id/7573247/height/90/theme/custom/autoplay/no/autonext/no/thumbnail/no/preload/no/no_addthis/no/direction/forward/render-playlist/no/custom-color/000000/" height="90" width="100%" scrolling="no"  allowfullscreen webkitallowfullscreen mozallowfullscreen oallowfullscreen msallowfullscreen></iframe>`
   },
   {
     title: "Episode 2",
     date: "July 12, 2018",
     description: "Lefty wonders about the possibilities of an artifact he found, but he needs his friends Corny Falco and Pavlovena to help identify what it is and what it does. Meanwhile the people of the Ark decide to build defenses to protect against the dangers of The Zone, when trouble strikes!<br><br>Original music composed by Max Beyer, and a big thanks to these other artists for their music found at freesound.org:<br><br>Sirkoto51 - Anime Fight Music Loop <br><br> neehnahw - Konflict",
-    audio: "http://media.blubrry.com/thenotoriousrpg/static1.squarespace.com/static/51f14daae4b0e3adff18a86a/t/5b46106b6d2a73ffbacd9539/1531318470818/Episode+2.mp3/original/Episode+2.mp3"
+    audio: `<iframe style="border: none" src="//html5-player.libsyn.com/embed/episode/id/7573253/height/90/theme/custom/autoplay/no/autonext/no/thumbnail/no/preload/no/no_addthis/no/direction/forward/render-playlist/no/custom-color/000000/" height="90" width="100%" scrolling="no"  allowfullscreen webkitallowfullscreen mozallowfullscreen oallowfullscreen msallowfullscreen></iframe>`
   },
   {
     title: "Episode 1",
     date: "July 12, 2018",
     description: "In this setup episode of The Notorious RPG, 4 Minneapolis friends and improvisers create their mutant characters in a dystopian post-apocalyptic future.  Join in the campaign as Patrick (Lefty), Maggie (Pavlovena) and Brandon (Corny Falco), led by Game Master Max, venture into the unknown.<br><br>Original music composed by Max Beyer.",
-    audio: "http://media.blubrry.com/thenotoriousrpg/static1.squarespace.com/static/51f14daae4b0e3adff18a86a/t/5b460f280e2e72b22160548c/1531318139855/Episode+1.mp3/original/Episode+1.mp3"
+    audio: `<iframe style="border: none" src="//html5-player.libsyn.com/embed/episode/id/7573256/height/90/theme/custom/autoplay/no/autonext/no/thumbnail/no/preload/no/no_addthis/no/direction/forward/render-playlist/no/custom-color/000000/" height="90" width="100%" scrolling="no"  allowfullscreen webkitallowfullscreen mozallowfullscreen oallowfullscreen msallowfullscreen></iframe>`
   }
 ];
 
@@ -67,9 +73,7 @@ function episodeTemplate(episode) {
       <h3>${episode.title}</h3>
       <p><i>${episode.date}</i></p>
       <p>${episode.description}</p>
-      <audio controls="controls" src="${episode.audio}">
-        Whoopsie Doopsie, an error occured!
-      </audio>
+      <p>${episode.audio}</p>
   `;
 }
 
